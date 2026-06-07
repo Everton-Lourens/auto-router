53e1d46b41d7925ab8d888a7405949e21981f005
Everton Lourens <123413870+Everton-Lourens@users.noreply.github.com>
2026-06-07 14:39:26 -0300

    Update screenshotHuawai.js

--- screenshotHuawai.js
+++ screenshotHuawai.js
@@ -107,6 +107,34 @@ var inputPassword = null;
       throw new Error('input[type=file] não encontrado');
     }
 
+
+    ///////////////////////////
+    ///////////////////////////
+
+const novoSSID = 'CLIENTE123';
+
+let xml = fs.readFileSync(
+  '/storage/emulated/0/Download/router/preDefault.html',
+  'utf8'
+);
+
+    await wait(1000);
+
+xml = xml.replaceAll('TRICOLOR', novoSSID);
+    
+await wait(1000);
+    
+fs.writeFileSync(
+  '/storage/emulated/0/Download/router/upHuawai.html',
+  xml
+);
+
+console.log('SSID alterado para:', novoSSID);
+
+    ///////////////////////////
+    ///////////////////////////
+    await wait(2000);
+
     console.log('[IMPORT] Iniciando upload...');
     await fileInput.uploadFile(
       '/storage/emulated/0/Download/router/upHuawai.html'
